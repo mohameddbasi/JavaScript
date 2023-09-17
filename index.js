65f3bf9a4336f814ebe86ace038e280730d9e4e3
mohameddbasi <mdmohamedbasi@gmail.com>
2023-09-17 23:08:02 +0530

    variables operators

---
+++ index.js
@@ -0,0 +1,120 @@
+/*const name= "basith";  //var let const
+const dob="04-09-2002";
+let msg="hello my name is " + name + " & i born on " + dob+ " I'm learning javascript";  //+(concadinate)
+
+//logging
+console.log(msg);
+
+
+
+let number1=45;
+let number2=5;
+let result=number1+number2;
+console.log(result);
+
+
+let firstName="basith";  //string
+
+let age= 23;   //number or integer
+
+let isyoung=true;   //boolean
+
+let lastName= undefined;   //undefined
+
+let trophy=null;   //null 
+
+
+
+let name="Mohamed Basith";
+let age=24;
+let gender="male";
+let address="thanjavur,tamilnadu,india";
+
+
+
+
+                   //object
+let person={
+    Name:"Mohamed Basith", //key=value
+    age:21,
+    isalive:"false",
+    gender:"male",
+    address:"thanjavur,tamilnadu,india",
+    siblings:{
+        brother:"A",
+        sister:"B",
+    }
+//object
+
+//person.Name="mohamed",
+
+//dot notation
+console.log(person.siblings.sister)
+
+//Bracket notation
+console.log(person['gender'])
+
+
+
+      
+
+
+              //array
+              
+let favcolors=["red","white","black"]
+
+favcolors[4]="green",
+//console.log(favcolors[1]);
+console.log(favcolors.length);
+
+
+
+         //function
+
+    Eg-1: function greetUser(firstName, lastName){
+       
+         let msg="hello my name is " + firstName + " " + lastName+ " I'm learning javascript";  //+(concadinate)
+         console.log(msg)
+    }    
+    greetUser("mohamed","basith")
+    greetUser("javith","akbar") 
+
+
+   Eg-2: function add(number1,number2){
+        console.log(number1+number2)   
+    }
+    add(45,500)*/
+
+
+
+         //operator
+
+   let number1=5;
+   let number2=20;
+
+   console.log(number1 + number2)
+   console.log(number1 - number2)
+   console.log(number1 * number2)
+   console.log(number1 / number2)
+   console.log(number1 % number2)
+   console.log(number1 ** number2)
+   
+   
+
+
+   
+    
+
+
+         
+              
+
+
+
+
+
+
+
+
+
+
