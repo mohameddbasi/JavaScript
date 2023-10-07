b6a488f1e9a9f56405bfb5e9d8a36396d0f40954
mohameddbasi <mdmohamedbasi@gmail.com>
2023-10-07 18:56:08 +0530

    form-methods

--- course-foodapp/learn.js
+++ course-foodapp/learn.js
@@ -1,151 +1,34 @@
-// const tableEL=document.querySelector("table")
+const formEl = document.forms.feedback;
+const btnEl = document.querySelector("form button");
 
-// let selectedId;
-// tableEL.addEventListener("click",(event) =>{
-//     const target=event.target
-//     const closestTr=target.closest("tr");
+const handleSubmit = (event) => {
+  event.preventDefault(); // Prevent default behaviour
 
-//     if(target.tagName==="TH")return;//ignoring TH
+  // construct a FormData object, which fires the formdata event
+  const formData = new FormData(formEl);
 
-// if(selectedId != undefined){
-//     selectedId.classList.remove("active");
-// }
-// selectedId=closestTr;
+  // formdata gets modified by the formdata event
+  console.log("My API KEY IS: ", formData.get("api-key"));
+};
 
-// closestTr.classList.add("active");
-//     alert(`hello ${closestTr.children[0].textContent}`);
-// })
+const handleFormData = (e) => {
+  console.log("formdata fired!");
 
-// //Task:show a donate page on Toogle
-// document.addEventListener("click",(event)=>{
-//     const id=event.target.dataset.toggleId;
-//     if (!id)return; //ignore all target place
-//     const el=document.getElementById(id);
-//     el.hidden=!el.hidden;
-// });
-// const formEl=document.querySelector("#donate-form")
+  const formData = e.formData;
 
-// formEl.addEventListener("submit",(event)=>{
-//     event.preventDefault();
-//    const donateamount= event.target.querySelector("input").value;
-//    console.log(donateamount);
-// alert(`Thanks you for donating $${donateamount}.`)
-// });
+  // useful methods
+  formData.append("api-key", "sadsadsadsadsadsa"); // append new key-value pairs
+  console.log([...formData.entries()]); // get entries
+  console.log([...formData.values()]); // get only values
+  console.log(formData.get("email")); // get individual form element's value
+  console.log(formData.getAll("type")); // returns an array of all the values by a key name
+  console.log(formData.has("gender")); // returns true if the FormData object contains a key name.
+  console.log([...formData.keys()]); // returns an iterator of all the keys.
+  formData.set("hobbies", "Learning new things"); //sets a new value for an existing key name
+  formData.delete("terms"); // deletes the key
+  console.log([...formData.values()]); //returns an iterator of all the values in the FormData object.
+};
 
+formEl.addEventListener("submit", handleSubmit);
 
-// document.addEventListener("mousedown",(event) => {
-//     console.log("Mousedown",event);
-// });
-
-// document.addEventListener("copy",(event)=>{
-//     event.preventDefault();
-//     alert("copying is prohibited")
-// })
-
-
-// document.addEventListener("mouseup",(event)=>{
-//     console.log("Mouseup",event);
-// })
-
-
-// document.addEventListener("mouseenter",(event)=>{
-//     console.log("Mouseenter");
-// })
-
-// document.addEventListener("mouseleave",(event)=>{
-//     console.log("Mouseleave");
-// })
-
-// document.addEventListener("mousemove",(event)=>{
-//         console.log("Mousemove");
-//  })
-
-// document.addEventListener("mouseout",(event)=>{
-//     console.log("Mouseout");
-// })
-
-// document.addEventListener("mouseover",(event)=>{
-//     console.log("Mouseover");
-// })
-
-
-// document.addEventListener("dblclick",(event)=>{
-//     console.log("dblclick");
-// })
-
-// const foodcontainerEl=document.getElementById("food-container")
-// foodcontainerEl.addEventListener("mouseenter",(event)=>{
-//     console.log("mouseenter",event);
-// })
-
-
-
-//Task:create an mouse tester app which should display respective messsage when that button is clicked.
-
-// document.addEventListener("mouseup",(event)=>{
-//     switch (event.which) {
-//         case 1:
-//             alert("right click");
-//             break;
-//         case 2:
-//             alert("middle click");
-//             break;
-//         case 3:
-//             alert("left click");
-//             break;  
-//         default:
-//             alert("ivalid or unknown detected")
-//             break;
-//     }
-// })
-
-
-//keyboard event
-
-// document.addEventListener("keydown",(event)=>{
-//     console.log("keydown",event);
-// })
-
-// document.addEventListener("keyup",(event)=>{
-//     console.log("keyup",event);
-// })
-
-
-
-
-
-//Task:build a simple typing-test screen
-// document.addEventListener("keyup",(event)=>{
-//     console.log("keyup",event);
-
-//     switch(event.key){
-//             case "Arrowup":
-//             alert("you have pressed Arrowup key");
-//             break;
-
-//             case "Arrowdown":
-//             alert("you have pressed Arrowdown key");
-//             break;
-
-//             case "Arrowleft":
-//             alert("you have pressed Arrowleft key");
-//             break;
-
-//             case "ArrowRight":
-//             alert("you have pressed Arrowright key");
-//             break;
-
-//           default:
-//             alert("unknown key detected");
-//             break;
-//     }
-// })
-
-
-
-
-
-
-
-
-
+formEl.addEventListener("formdata", handleFormData);
